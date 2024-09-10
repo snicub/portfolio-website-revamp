cc1e9296325133917e719c85717a2693b5a5e219
Daniel Han <daniel.hangb@gmail.com>
2024-09-10 00:12:57 -0400

    image loading faster, not fixed

--- src/Components/SpecificLandingPage/PLP.tsx
+++ src/Components/SpecificLandingPage/PLP.tsx
@@ -22,19 +22,26 @@ const PLP: React.FC = () => {
 
     const fadeTimeout = setTimeout(() => {
       setFadeOut(true); // Trigger fade-in
-    }, 100); // Small delay for fade-in
+    }, 0); // Small delay for fade-in
 
     return () => clearTimeout(fadeTimeout); // Clean up the timer
   }, []); // Empty dependency ensures this runs only on mount (for page refresh)
 
-  // Handle fade effect when location.state changes (for navigation)
+  const preloadImage = (src: string) => {
+    const img = new Image();
+    img.src = src;
+  };
+
   useEffect(() => {
     setFadeOut(true); // Start fade-out effect
+
+    // Preload the next image
+    preloadImage(newContent.imageSrc);
 
     const fadeTimeout = setTimeout(() => {
       setContent(newContent); // Update the content after fade-out completes
       setFadeOut(false); // Start the fade-in effect
-    }, 750); // Adjust the timing for the fade-out effect duration
+    }, 1000); // Adjust the timing for the fade-out effect duration
 
     return () => clearTimeout(fadeTimeout); // Clean up the timer on unmount
   }, [location.state, newContent]);
@@ -44,78 +51,80 @@ const PLP: React.FC = () => {
   const { imageSrc, altText, title, index, info, plpImages } = content;
 
   return (
-    <div
-      className="entire-plp"
-      style={{
-        marginTop: "50px",
-        opacity: fadeOut ? 0 : 1, // Fades out before content change, then fades in
-        transition: "opacity .75s ease-in-out", // Smooth fade effect with ease-in timing
-      }}
-    >
+    <>
       <div className="bottom-navbar-wrapper">
         <Bottombar index={index} />
       </div>
 
       <div
-        className="info-card"
+        className="entire-plp"
         style={{
-          display: "flex",
-          gap: "60px",
-          flexDirection: "column",
-          padding: "30px",
+          marginTop: "50px",
+          opacity: fadeOut ? 0 : 1, // Fades out before content change, then fades in
+          transition: "opacity 1s ease-in-out", // Smooth fade effect with ease-in timing
         }}
       >
         <div
-          className="title-wrapper"
+          className="info-card"
           style={{
-            justifyContent: "center",
             display: "flex",
-            fontFamily: "favorit",
-            fontSize: "2rem",
-            fontStyle: "normal",
-            fontWeight: "bold",
-            textAlign: "center",
+            gap: "60px",
+            flexDirection: "column",
+            padding: "30px",
           }}
         >
-          {title}
-        </div>
-        <div className="content-wrapper">
           <div
-            className="image-and-info-wrapper"
+            className="title-wrapper"
             style={{
+              justifyContent: "center",
               display: "flex",
-              flexDirection: isMobile ? "column" : "row",
-              alignItems: "center",
-              gap: "10px",
+              fontFamily: "favorit",
+              fontSize: "2rem",
+              fontStyle: "normal",
+              fontWeight: "bold",
+              textAlign: "center",
             }}
           >
-            <img
-              loading="eager"
-              className="mainImage"
-              style={{
-                maxWidth: !isMobile ? "50%" : undefined,
-                height: "auto",
-                display: "block",
-                width: "100%",
-              }}
-              src={`${imageSrc}?${new Date().getTime()}`} // Force reload to avoid cache issue
-              alt={altText}
-            />
+            {title}
+          </div>
+          <div className="content-wrapper">
             <div
-              className="info-wrapper"
+              className="image-and-info-wrapper"
               style={{
-                fontFamily: "favorit",
-                fontSize: "1rem",
-                fontStyle: "normal",
+                display: "flex",
+                flexDirection: isMobile ? "column" : "row",
+                alignItems: "center",
+                gap: "10px",
               }}
             >
-              {info}
+              <img
+                loading={content === newContent ? "eager" : "lazy"} // Only eager load on first load
+                className="mainImage"
+                style={{
+                  maxWidth: !isMobile ? "50%" : undefined,
+                  height: "auto",
+                  display: "block",
+                  width: "100%",
+                }}
+                src={`${imageSrc}?${new Date().getTime()}`} // Force reload to avoid cache issue
+                alt={altText}
+              />
+              <div
+                className="info-wrapper"
+                style={{
+                  fontFamily: "favorit",
+                  fontSize: "1rem",
+                  fontStyle: "normal",
+                }}
+              >
+                {info}
+              </div>
             </div>
           </div>
+          <Collage plpImages={plpImages} />
         </div>
-        <Collage plpImages={plpImages} />
       </div>
-    </div>
+    </>
   );
 };
 
