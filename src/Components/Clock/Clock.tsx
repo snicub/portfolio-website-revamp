16e2d329f66d5656113f80aa4b470745eecdd441
Daniel Han <daniel.hangb@gmail.com>
2024-09-17 12:50:03 -0400

    remove layoutshift checks cuz render blanks

--- src/Components/Clock/Clock.tsx
+++ src/Components/Clock/Clock.tsx
@@ -33,7 +33,6 @@ const Clock = () => {
     return () => clearInterval(timerId);
   }, []);
 
-  if (!isMobile) return null;
   return (
     <div style={{ fontSize: isMobile ? ".75rem" : undefined }}>{ctime}</div>
   );
