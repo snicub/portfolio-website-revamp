dfe7ad121761bc79fc7befd3e38b193f3e65a492
Daniel Han <daniel.hangb@gmail.com>
2025-04-20 11:22:27 -0400

    compromised api key

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyArWKMTuqkt4Yfj1f1G_OhoSvx6JhD5CW8",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "daniel-han-portfolio.firebaseapp.com",
   projectId: "daniel-han-portfolio",
   storageBucket: "daniel-han-portfolio.firebasestorage.app",
   messagingSenderId: "587413620340",
-  appId: "1:587413620340:web:eb24e5d206ef81e7333321",
-  measurementId: "G-YS2FG74L0C",
+  appId: "1:587413620340:web:8092bd387f724d65333321",
+  measurementId: "G-8D2MPCCP51",
 };
 
 // Initialize Firebase
