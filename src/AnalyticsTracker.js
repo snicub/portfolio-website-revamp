1d415849cdb868a3aa0a914f4f26cb5af28eac69
Daniel Han <daniel.hangb@gmail.com>
2025-04-29 18:29:09 -0400

    add device details

--- src/AnalyticsTracker.js
+++ src/AnalyticsTracker.js
@@ -1,19 +1,32 @@
 // src/AnalyticsTracker.js
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { logEvent } from "firebase/analytics";
+import { logEvent, setUserProperties } from "firebase/analytics";
 import { analytics } from "./firebase";
 
 export default function AnalyticsTracker() {
   const location = useLocation();
 
   useEffect(() => {
-    if (analytics) {
-      logEvent(analytics, "screen_view", {
-        screen_name: location.pathname,
-        screen_class: location.pathname,
-      });
-    }
+    if (!analytics) return;
+
+    // Gather values
+    const screenName = location.pathname;
+    const screenClass = location.pathname;
+    const ua = navigator.userAgent;
+    const isMobile = /Mobi|Android/i.test(ua);
+    const deviceType = isMobile ? "mobile" : "desktop";
+
+    // Set user property
+    setUserProperties(analytics, { device_type: deviceType });
+
+    // Log Firebase event
+    logEvent(analytics, "screen_view", {
+      screen_name: screenName,
+      screen_class: screenClass,
+      device_type: deviceType,
+      user_agent: ua,
+    });
   }, [location]);
 
   return null;
