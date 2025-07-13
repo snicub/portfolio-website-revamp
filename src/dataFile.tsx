395e7abca888318a3e3434fdcfd6e8c70bea740f
Daniel Han <daniel.hangb@gmail.com>
2025-07-13 15:54:10 -0400

    update assets

--- src/dataFile.tsx
+++ src/dataFile.tsx
@@ -4,6 +4,10 @@ const Data = {
       mobileSrc: require("./Assets/enterpage/danandmom.webp"),
     },
     {
+      mobileSrc: require("./Assets/enterpage/sniblap.webp"),
+      desktopSrc: require("./Assets/enterpage/desktopAndTablet/sniblapDAT.png"),
+    },
+    {
       mobileSrc: require("./Assets/enterpage/rock.webp"),
       desktopSrc: require("./Assets/enterpage/desktopAndTablet/rockDesktop.png"),
     },
@@ -94,30 +98,16 @@ const Data = {
         { src: require("./Assets/plp/cooking/food4.webp") },
         { src: require("./Assets/plp/cooking/rolls.webp") },
         { src: require("./Assets/plp/cooking/bak1.jpg") },
-        { src: require("./Assets/plp/cooking/bak2.jpg") },
-
-        // Add more images as needed
+        { src: require("./Assets/plp/cooking/bowl.webp") },
+        { src: require("./Assets/plp/cooking/bowl2.webp") },
+        { src: require("./Assets/plp/cooking/bowl3.webp") },
       ],
       altText: "dan han cooking",
       title: "in the lab",
       info: "In my free time, I enjoy cooking different recipes and learning new techniques. I also love feeding my friends and family. Some of my best memories come from breaking bread with others and I want to be able to provide that for those around me.",
     },
-    // {
-    //   img: require("./Assets/gallery/nestle.webp"),
-    //   plpImages: [
-    //     { src: require("./Assets/plp/nespresso/nes1.webp") },
-    //     { src: require("./Assets/plp/nespresso/nes2.webp") },
-    //     { src: require("./Assets/plp/nespresso/nes3.webp") },
-    //     { src: require("./Assets/plp/nespresso/nes4.webp") },
-    //     { src: require("./Assets/plp/nespresso/nes5.webp") },
-    //     // Add more images as needed
-    //   ],
-    //   altText: "nespresso internship",
-    //   title: "nespresso",
-    //   info: "The past Summer I interned at Nespresso as a front end developer. During my time here, I was able to polish my React skills which helped me make this website. I created two main components for their website redesign that I'm super proud of. I didn't think I would be able to make such an impact as an intern, but we defy the limits here. And yes, I drank a lotttt of free coffee!!",
-    // },
     {
-      img: require("./Assets/plp/fam/fam11.JPG"),
+      img: require("./Assets/plp/fam/aurassert.webp"),
       //img: require("./Assets/gallery/danfam.webp"),
       plpImages: [
         { src: require("./Assets/plp/fam/lucas.webp") },
@@ -145,7 +135,7 @@ const Data = {
       img: require("./Assets/gallery/twon.webp"),
       plpImages: [
         { src: require("./Assets/plp/friends/alan.webp") },
-        { src: require("./Assets/plp/friends/elaine.webp") },
+        // { src: require("./Assets/plp/friends/elaine.webp") },
         { src: require("./Assets/plp/friends/point.webp") },
         { src: require("./Assets/plp/friends/pointban.webp") },
         { src: require("./Assets/plp/friends/vish.webp") },
@@ -180,7 +170,7 @@ const Data = {
       ],
       altText: "working at tbell",
       title: "humble beginnings",
-      info: "My first job was working at Taco Bell as a steamer on the line. It holds a special place as the Taco Bell Foundation has funded my college education as well and working there taught me valuable skills. Don't forget to live mas.",
+      info: "My first job was working at Taco Bell as a steamer on the line. It holds a special place as the Taco Bell Foundation has funded my college education. Don't forget to live mas.",
     },
     {
       img: require("./Assets/gallery/dough.webp"),
@@ -208,66 +198,66 @@ const Data = {
       title: "sourdough",
       info: "I got into sourdough baking this past summer! I love the process of making a loaf from start to finish. And no, I haven't named my starter yet.. lol. Sourdough bread making is super appealing to me and such a rewarding process. I'm trying to make homemade croissants next!!",
     },
-    {
-      img: require("./Assets/gallery/coffee.webp"),
-      plpImages: [
-        { src: require("./Assets/plp/coffee/matcha.webp") },
-        { src: require("./Assets/plp/coffee/coffee.webp") },
-        { src: require("./Assets/plp/coffee/esp.webp") },
-        { src: require("./Assets/plp/coffee/stump.webp") },
+    // {
+    //   img: require("./Assets/gallery/coffee.webp"),
+    //   plpImages: [
+    //     { src: require("./Assets/plp/coffee/matcha.webp") },
+    //     { src: require("./Assets/plp/coffee/coffee.webp") },
+    //     { src: require("./Assets/plp/coffee/esp.webp") },
+    //     { src: require("./Assets/plp/coffee/stump.webp") },
 
-        // Add more images as needed
-      ],
-      altText: "fresh coffee",
-      title: "morning cup",
-      info: "I love coffee and enjoy trying new roasts and brewing methods. Right now I'm using a timemore c2 grinder and aeropress. I have a pourover set but I don't use it too often. Caffeine 90min after waking up huberman tech?",
-    },
-    {
-      img: require("./Assets/plp/golf/dantee.webp"),
-      plpImages: [
-        { src: require("./Assets/plp/golf/danGolf.webp") },
-        { src: require("./Assets/plp/golf/danGolfFam.webp") },
-        { src: require("./Assets/plp/golf/golf1.webp") },
+    //     // Add more images as needed
+    //   ],
+    //   altText: "fresh coffee",
+    //   title: "morning cup",
+    //   info: "I love coffee and enjoy trying new roasts and brewing methods. Right now I'm using a timemore c2 grinder and aeropress. I have a pourover set but I don't use it too often. Caffeine 90min after waking up huberman tech?",
+    // },
+    // {
+    //   img: require("./Assets/plp/golf/dantee.webp"),
+    //   plpImages: [
+    //     { src: require("./Assets/plp/golf/danGolf.webp") },
+    //     { src: require("./Assets/plp/golf/danGolfFam.webp") },
+    //     { src: require("./Assets/plp/golf/golf1.webp") },
 
-        // Add more images as needed
-      ],
-      altText: "dan like golf",
-      title: "golf",
-      info: "In my free time, I also like to play golf. I got into it because my dad loves playing, and I just wanted to spend more quality time with him. I still suck and top most of my tee offs.",
-    },
-    {
-      img: require("./Assets/plp/korea/korea13.webp"),
-      plpImages: [
-        { src: require("./Assets/plp/korea/korea1.webp") },
-        { src: require("./Assets/plp/korea/korea2.webp") },
-        { src: require("./Assets/plp/korea/korea3.webp") },
-        { src: require("./Assets/plp/korea/korea4.webp") },
-        { src: require("./Assets/plp/korea/korea5.webp") },
-        { src: require("./Assets/plp/korea/korea6.webp") },
-        { src: require("./Assets/plp/korea/korea7.webp") },
-        { src: require("./Assets/plp/korea/korea8.webp") },
-        { src: require("./Assets/plp/korea/korea9.webp") },
-        { src: require("./Assets/plp/korea/korea10.webp") },
-        { src: require("./Assets/plp/korea/korea11.webp") },
-        { src: require("./Assets/plp/korea/korea12.webp") },
-        { src: require("./Assets/plp/korea/korea.webp") },
-        { src: require("./Assets/plp/korea/korea14.webp") },
-        { src: require("./Assets/plp/korea/korea15.webp") },
-        { src: require("./Assets/plp/korea/korea16.webp") },
+    //     // Add more images as needed
+    //   ],
+    //   altText: "dan like golf",
+    //   title: "golf",
+    //   info: "In my free time, I also like to play golf. I got into it because my dad loves playing, and I just wanted to spend more quality time with him. I still suck and top most of my tee offs.",
+    // },
+    // {
+    //   img: require("./Assets/plp/korea/korea13.webp"),
+    //   plpImages: [
+    //     { src: require("./Assets/plp/korea/korea1.webp") },
+    //     { src: require("./Assets/plp/korea/korea2.webp") },
+    //     { src: require("./Assets/plp/korea/korea3.webp") },
+    //     { src: require("./Assets/plp/korea/korea4.webp") },
+    //     { src: require("./Assets/plp/korea/korea5.webp") },
+    //     { src: require("./Assets/plp/korea/korea6.webp") },
+    //     { src: require("./Assets/plp/korea/korea7.webp") },
+    //     { src: require("./Assets/plp/korea/korea8.webp") },
+    //     { src: require("./Assets/plp/korea/korea9.webp") },
+    //     { src: require("./Assets/plp/korea/korea10.webp") },
+    //     { src: require("./Assets/plp/korea/korea11.webp") },
+    //     { src: require("./Assets/plp/korea/korea12.webp") },
+    //     { src: require("./Assets/plp/korea/korea.webp") },
+    //     { src: require("./Assets/plp/korea/korea14.webp") },
+    //     { src: require("./Assets/plp/korea/korea15.webp") },
+    //     { src: require("./Assets/plp/korea/korea16.webp") },
 
-        // Add more images as needed
-      ],
-      altText: "dan loves korea",
-      title: "korea",
-      info: "I've traveled to Korea two times!! First being a study abroad program at Ewha womans university in Seoul, where I participated in their summer co-ed program. I worked on a personal research project, interned at a think tank, and took a Korean elective course on neoliberalism. The following summer I went to Korea again, this time with my sister to travel. We went to Daegu, Busan, Gyeongju, Jeonju, and Jeju. We also spent quality time with our relatives in Seoul. I think it goes without saying that their food is amazing and super affordable. Really missing that!!  ",
-    },
+    //     // Add more images as needed
+    //   ],
+    //   altText: "dan loves korea",
+    //   title: "korea",
+    //   info: "I've traveled to Korea two times!! First being a study abroad program at Ewha womans university in Seoul, where I participated in their summer co-ed program. I worked on a personal research project, interned at a think tank, and took a Korean elective course on neoliberalism. The following summer I went to Korea again, this time with my sister to travel. We went to Daegu, Busan, Gyeongju, Jeonju, and Jeju. We also spent quality time with our relatives in Seoul. I think it goes without saying that their food is amazing and super affordable. Really missing that!!  ",
+    // },
   ],
 
   aboutSection: {
     card1: {
       title: "About Me",
       content:
-        "Hi! I'm Dan, a software engineer based in New Jersey. I have a passion for creating and building things. I specialize in front end development, hoping to provide the best user experience possible. I currently study computer science and Korean at Rutgers University in NJ.",
+        "Hi! I'm Dan, a software engineer based in New Jersey. Currently building Nespresso.com. I have a passion for creating platforms and services that make people's lives easier. ",
     },
   },
   programmingSection: [
